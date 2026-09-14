e1f9704f6f18dcf592afb785f2750d8ee988f7a1
NadaliN2 <bruno.h.nadalin@gmail.com>
2026-09-14 20:46:42 -0300

    Refactor approval logic based on grade and attendance

--- aprovacao.c
+++ aprovacao.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 
 int main() {
-	float A1, A2, media;
-	printf("Digite a primeira nota do aluno: ");
-	scanf("%f", &A1);
-	printf("Digite a segunda nota do aluno: ");
-	scanf("%f", &A2);
+	float nota, frequencia;
 	
-	media = (A1 + A2) / 2;
+	printf("Digite sua nota: ");
+	scanf("%f", &nota);
+	printf("Digite sua frequencia: ");
+	scanf("%f", &frequencia);
 	
-	if (media >= 6){
-		printf("Aprovado!");
-	}	else	{
-		printf("Reprovado!");
+	if (nota >= 9 && frequencia >= 90) {
+		printf("Media: %f\n", nota);
+		printf("Media: %f\n", frequencia);
+		printf("Resultado: Bolsa Integral");
+	} else if (nota >= 7 && frequencia >= 70) {
+		printf("Media: %f\n", nota);
+		printf("Media: %f\n", frequencia);
+		printf("Resultado: Bolsa Parcial");
+	} else if (nota < 7 && frequencia < 70) {
+		printf("Media: %f\n", nota);
+		printf("Media: %f\n", frequencia);
+		printf("Resultado: Sem Bolsa");
 	}
 }
+
